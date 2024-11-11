d45330ae5511cb7be84a908501e46dc2b6fe6c5d
Anubhav Gupta <anubhavgu2002@gmail.com>
2024-11-11 18:17:41 +0530

    changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,14 @@
-const User = require('../models/User');
-const generateToken = require('../utils/jwt');
+const User = require("../models/User");
+const generateToken = require("../utils/jwt");
 
 exports.register = async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = new User({ username, password });
     await user.save();
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    res.status(500).json({ message: 'User registration failed', error });
+    res.status(500).json({ message: "User registration failed", error });
   }
 };
 
@@ -17,11 +17,17 @@ exports.login = async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
     if (!user || !(await user.comparePassword(password))) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
     const token = generateToken(user._id);
-    res.json({ token });
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      })
+      .json({ token });
   } catch (error) {
-    res.status(500).json({ message: 'User login failed', error });
+    res.status(500).json({ message: "User login failed", error });
   }
 };
